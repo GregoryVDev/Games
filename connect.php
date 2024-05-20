5042b3dc73b6cb43acc28dfb71b8bad103aa9fb6
GregoryDev <119239831+GregoryVDev@users.noreply.github.com>
2024-05-20 17:14:34 +0200

    create the connect.php ans style.css and added try and catch

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+try{
+    $server_name = "localhost";
+    $db_name = "games";
+    $user_name = "root";
+    $password = "root";
+    $db = new PDO("mysql:host=$server_name;dbname=$db_name;charset=utf8mb4", "$user_name", "$password");
+}catch(PDOException $e){
+    echo "echec de connexion" . " ". $e->getMessage();
+};
+
+?>
